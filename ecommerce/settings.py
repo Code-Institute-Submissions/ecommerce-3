bf24caee37c21a923763aec73e0f57f88440110d
Ubuntu <ubuntu@ip-172-31-84-233.ec2.internal>
2019-09-05 12:52:43 +0000

    working ecommerce django project

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,7 +138,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=94608000',
 }
 
-AWS_STORAGE_BUCKET_NAME = 'jmc-ecommerce'
+AWS_STORAGE_BUCKET_NAME = 'jmcs-ecommerce'
 AWS_S3_REGION_NAME = 'eu-west-1'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
@@ -152,6 +152,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
