a382c9d6c6bb99c703f6d38853d5f30e704eabb2
Ubuntu <ubuntu@ip-172-31-84-233.ec2.internal>
2019-09-07 19:16:45 +0000

    adding index to url's

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from .views import index, register, profile, logout, login
 
 urlpatterns = [
     url(r'^register/$', register, name='register'),
+    url(r'^index/$', index, name='index'),
     url(r'^profile/$', profile, name='profile'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^login/$', login, name='login'),
